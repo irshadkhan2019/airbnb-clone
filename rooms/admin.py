1d85c5fa4ca1024cc9fde684c50012c1acbad37e
Irshad Khan <khanirshad2019@gmail.com>
2023-04-03 09:43:26 +0530

    Room admin panel

--- rooms/admin.py
+++ rooms/admin.py
@@ -6,7 +6,13 @@ from . import models
 class ItemAdmin(admin.ModelAdmin):
     """Item Admin Definition"""
 
-    pass
+    list_display = (
+        "name",
+        "used_by",
+    )
+
+    def used_by(self, obj):
+        return obj.rooms.count()
 
 
 @admin.register(models.Photo)
@@ -16,4 +22,95 @@ class PhotoAdmin(admin.ModelAdmin):
 
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
-    pass
+
+    fieldsets = (
+        (
+            "Basic Info",
+            {
+                "fields": (
+                    "name",
+                    "description",
+                    "country",
+                    "address",
+                    "price",
+                )
+            },
+        ),
+        (
+            "Times",
+            {
+                "fields": (
+                    "check_in",
+                    "check_out",
+                    "instant_book",
+                )
+            },
+        ),
+        (
+            "Spaces",
+            {
+                "fields": (
+                    "beds",
+                    "bathrooms",
+                    "baths",
+                    "guests",
+                )
+            },
+        ),
+        (
+            "More About Space",
+            {
+                "classes": ("collapse",),
+                "fields": (
+                    "amenities",
+                    "facilities",
+                    "house_all_rules",
+                ),
+            },
+        ),
+        (
+            "Last Details",
+            {"fields": ("host",)},
+        ),
+    )
+
+    list_display = (
+        "name",
+        "description",
+        "city",
+        "price",
+        "beds",
+        "bathrooms",
+        "baths",
+        "guests",
+        "check_in",
+        "check_out",
+        "instant_book",
+        "count_amenities",
+        "count_photos",
+    )
+    list_filter = (
+        "instant_book",
+        "room_type",
+        "amenities",
+        "facilities",
+        "house_all_rules",
+        "host__superhost",
+        "city",
+        "country",
+    )
+    search_fields = ("=city", "^host__username")
+
+    ordering = ("name", "price")
+
+    filter_horizontal = (
+        "amenities",
+        "facilities",
+        "house_all_rules",
+    )
+
+    def count_amenities(self, obj):
+        return obj.amenities.count()
+
+    def count_photos(self, obj):
+        return obj.photos.count()
