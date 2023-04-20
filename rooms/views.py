6c8bf049f35428d8f91dc6e671f0be8fc7d5505a
Irshad Khan <khanirshad2019@gmail.com>
2023-04-20 11:06:47 +0530

    upload photo

--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,5 @@
 from django.http import Http404
-from django.views.generic import ListView, DetailView, View, UpdateView
+from django.views.generic import ListView, DetailView, View, UpdateView, FormView
 from django.shortcuts import render, redirect, reverse
 from django_countries import countries
 from django.core.paginator import Paginator
@@ -168,5 +168,18 @@ class EditPhotoView(user_mixins.LoggedInOnlyView, SuccessMessageMixin, UpdateVie
     fields = ("caption",)
 
     def get_success_url(self):
+        print(self.kwargs)
         room_pk = self.kwargs.get("room_pk")
         return reverse("rooms:photos", kwargs={"pk": room_pk})
+
+
+class AddPhotoView(user_mixins.LoggedInOnlyView, FormView):
+    template_name = "rooms/photo_create.html"
+    form_class = forms.CreatePhotoForm
+
+    def form_valid(self, form):
+        # pass room id to form
+        pk = self.kwargs.get("pk")
+        form.save(pk)
+        messages.success(self.request, "Photo Uploaded")
+        return redirect(reverse("rooms:photos", kwargs={"pk": pk}))
