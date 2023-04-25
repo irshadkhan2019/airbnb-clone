d1b20aaf8e0e6df058ba29a886978d15e343047f
Irshad Khan <khanirshad2019@gmail.com>
2023-04-25 13:05:59 +0530

    add room to favs/lists

--- lists/views.py
+++ lists/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, reverse
+from django.views.generic import TemplateView
+from rooms import models as room_models
+from . import models
 
-# Create your views here.
+
+@login_required
+def toggle_room(request, room_pk):
+    action = request.GET.get("action", None)
+    room = room_models.Room.objects.get_or_none(pk=room_pk)
+    if room is not None and action is not None:
+        the_list, _ = models.List.objects.get_or_create(
+            user=request.user, name="My Favourites Houses"
+        )
+        # add in many to many field rooms via add()
+        if action == "add":
+            the_list.rooms.add(room)
+        elif action == "remove":
+            # remove from many to many field rooms via remove()
+            the_list.rooms.remove(room)
+    return redirect(reverse("rooms:detail", kwargs={"pk": room_pk}))
